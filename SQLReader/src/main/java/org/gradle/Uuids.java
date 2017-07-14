b6d1664703151c910de3a7c18d521576175499b3
zuolang <zuolang@hotmail.com>
2017-07-15 00:14:50 +0800

    new file:   ../SQLReader/src/main/java/org/gradle/Uuids.java

---
+++ SQLReader/src/main/java/org/gradle/Uuids.java
@@ -0,0 +1,10 @@
+package org.gradle;
+
+public class Uuids {
+	public static String uuid(){
+		return java.util.UUID.randomUUID().toString();
+	}
+	public static void main(String[] args) {
+		System.out.println(uuid().length());
+	}
+}
