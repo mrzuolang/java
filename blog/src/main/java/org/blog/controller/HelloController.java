638335196e8683ea76154b23de8d1b243fa07cf2
zuolang <zuolang@hotmail.com>
2018-02-06 18:02:29 +0800

    controller test

--- blog/src/main/java/org/blog/controller/HelloController.java
+++ blog/src/main/java/org/blog/controller/HelloController.java
@@ -1,12 +1,28 @@
 package org.blog.controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("Hello")
+@RequestMapping("hello")
 public class HelloController {
 	@RequestMapping(value="sayHello")
 	public String sayHello() {
 		return "Hi hello .";
+	}
+	@RequestMapping(value="say")
+	public String say(@RequestParam String name) {
+		return name+" Hello!";
+	}
+	
+	@GetMapping("/insert")
+	public String insert(@RequestParam String hello ) {
+		
+		return hello;
+	}
+	@GetMapping("/save")
+	public String save(String hello,String name ) {
+		
+		return hello+name;
 	}
 }
