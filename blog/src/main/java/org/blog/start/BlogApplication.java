171fda950276a669d28aada0ab8d65695824665b
zuolang <zuolang@hotmail.com>
2018-02-07 00:27:31 +0800

    redis-conf

--- blog/src/main/java/org/blog/start/BlogApplication.java
+++ blog/src/main/java/org/blog/start/BlogApplication.java
@@ -3,9 +3,12 @@ package org.blog.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan("org.blog.controller")
+@ImportResource(locations={"classpath:spring/spring-redis.xml"})
+
 public class BlogApplication {
 
 	public static void main(String[] args) {
---
+++ blog/src/main/java/org/blog/util/UUIDFactory.java
@@ -0,0 +1,17 @@
+package org.blog.util;
+
+import java.util.UUID;
+
+/**
+ * @author lang
+ * UUID生成类
+ */
+public class UUIDFactory {
+	/**
+	 * 随机UUID大写
+	 * @return DD71AE88-6425-4E3E-84AE-3387C531942E
+	 */
+	public static String getUUID() {
+		return UUID.randomUUID().toString().toUpperCase();
+	}
+}	
